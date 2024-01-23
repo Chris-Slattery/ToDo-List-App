218489e76b3044bd98bd347fbe6ab8265539723e
Chris Slattery <chrisslattery08@gmail.com>
2024-01-23 13:16:58 +0000

    Continued working on front end

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import {MdOutlineDeleteOutline, MdEditNote, MdOutlineCheckBox, MdOutlineCheckBoxOutlineBlank} from 'react-icons/md'
 
-const Table = () => {
+const Table = ({ todos, setTodos, isLoading }) => {
   return (
-    <div className='py-32'>
+    <div className='py-2'>
       <table className='w-11/12 max-w-4x1'>
         <thead className='border-b-2 border-black'>
           <tr>
-            <th className='p-3 text-sm font-semibold tracking-wide'>Checkbox</th>
-            <th className='p-3 text-sm font-semibold tracking-wide'>To Do</th>
+            <th className='p-3 text-sm font-semibold tracking-wide text-left'>Checkbox</th>
+            <th className='p-3 text-sm font-semibold tracking-wide text-left'>To Do</th>
             <th className='p-3 text-sm font-semibold tracking-wide'>Status</th>
-            <th className='p-3 text-sm font-semibold tracking-wide'>Data Created</th>
-            <th className='p-3 text-sm font-semibold tracking-wide'>Actions</th>
+            <th className='p-3 text-sm font-semibold tracking-wide text-left'>Date Created</th>
+            <th className='p-3 text-sm font-semibold tracking-wide text-left'>Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {isLoading ? <div>Is Loading</div> :
+          <>
+          {todos.map( (todoItem, index) => {
+            return (
+              <tr key={todoItem.id} className='border-b border-black'>
             <td className='p-3'>
             <span className='inline-block cursor-pointer'><MdOutlineCheckBox /></span>
             </td>
-            <td className='p-3 text-sm'>Lorem ipsum dolor sit</td>
+            <td className='p-3 text-sm'>{todoItem.body}</td>
             <td className='p-3 text-sm text-center'>
             <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>Done</span>
             </td>
@@ -29,62 +33,9 @@ const Table = () => {
               <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline /></span>
             </td>
           </tr>
-          <tr>
-            <td className='p-3'>
-            <span className='inline-block cursor-pointer'><MdOutlineCheckBox /></span>
-            </td>
-            <td className='p-3 text-sm'>Lorem ipsum dolor sit</td>
-            <td className='p-3 text-sm text-center'>
-            <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>Done</span>
-            </td>
-            <td className='p-3 text-sm'>16-01-2024</td>
-            <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
-              <span className='text-xl cursor-pointer'><MdEditNote /></span>
-              <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline /></span>
-            </td>
-          </tr>
-          <tr>
-            <td className='p-3'>
-            <span className='inline-block cursor-pointer'><MdOutlineCheckBox /></span>
-            </td>
-            <td className='p-3 text-sm'>Lorem ipsum dolor sit</td>
-            <td className='p-3 text-sm text-center'>
-            <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>Done</span>
-            </td>
-            <td className='p-3 text-sm'>16-01-2024</td>
-            <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
-              <span className='text-xl cursor-pointer'><MdEditNote /></span>
-              <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline /></span>
-            </td>
-          </tr>
-          <tr>
-            <td className='p-3'>
-            <span className='inline-block cursor-pointer'><MdOutlineCheckBox /></span>
-            </td>
-            <td className='p-3 text-sm'>Lorem ipsum dolor sit</td>
-            <td className='p-3 text-sm text-center'>
-            <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>Done</span>
-            </td>
-            <td className='p-3 text-sm'>16-01-2024</td>
-            <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
-              <span className='text-xl cursor-pointer'><MdEditNote /></span>
-              <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline /></span>
-            </td>
-          </tr>
-          <tr>
-            <td className='p-3'>
-            <span className='inline-block cursor-pointer'><MdOutlineCheckBox /></span>
-            </td>
-            <td className='p-3 text-sm'>Lorem ipsum dolor sit</td>
-            <td className='p-3 text-sm text-center'>
-            <span className='p-1.5 text-xs font-medium tracking-wider rounded-md bg-green-300'>Done</span>
-            </td>
-            <td className='p-3 text-sm'>16-01-2024</td>
-            <td className='p-3 text-sm font-medium grid grid-flow-col items-center mt-5'>
-              <span className='text-xl cursor-pointer'><MdEditNote /></span>
-              <span className='text-xl cursor-pointer'><MdOutlineDeleteOutline /></span>
-            </td>
-          </tr>
+            )          
+          })
+          }</>}
         </tbody>
       </table>
     </div>
--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const TodoForm = () => {
   return (
     <div>
-      <p>Hello, World! This is TodoForm Component</p>
+      <input type="text" id="todo" placeholder="Add ToDo" className="input input-bordered input-info w-full max-w-xs"/>
+      <button className="btn btn-primary ml-2">Add ToDo</button>
     </div>
   );
 }
