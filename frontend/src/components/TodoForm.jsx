24705af17e7232f6e80d0e175df4b021c95171c8
Chris Slattery <chrisslattery08@gmail.com>
2024-02-26 12:27:27 +0000

    Began CRUD functionality

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const TodoForm = () => {
+
+  const [newTodo, setNewTodo] = useState({
+    'body': ''
+  })
+
+  const handleChange = (e) => {
+    setNewTodo(prev => ({
+      ...prev,
+      'body': e.target.value
+    }))
+    console.log(newTodo);
+  }
   return (
     <div>
-      <input type="text" id="todo" placeholder="Add ToDo" className="input input-bordered input-info w-full max-w-xs"/>
+      <input type="text" id="todo" placeholder="Add ToDo" className="input input-bordered input-info w-full max-w-xs"
+      onChange={handleChange} value={newTodo.body}/>
       <button className="btn btn-primary ml-2">Add ToDo</button>
     </div>
   );
